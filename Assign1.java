d335a70e041c6620d66c5b7909aafa8e1f100bb5
Samallll <samalchokli@gmail.com>
2023-06-30 17:32:07 +0530

    Updated first

---
+++ Assign1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Assign1{
+    public static void main(String[] args) {
+
+        Scanner obj= new Scanner(System.in);
+        char name = obj.next().charAt(0);
+
+        System.out.println("Your Name is\n"+name);  
+    }
+
+
+}
