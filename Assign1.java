29a084c19afee0c1952ca22c5464204dd8992c84
Samallll <samalchokli@gmail.com>
2023-06-30 17:39:19 +0530

    Assignment one completed

--- Assign1.java
+++ Assign1.java
@@ -4,6 +4,8 @@ class Assign1{
     public static void main(String[] args) {
 
         Scanner obj= new Scanner(System.in);
+        System.out.println("Enter the character");
+
         char name = obj.next().charAt(0);
 
         System.out.println("Your Name is\n"+name);  
