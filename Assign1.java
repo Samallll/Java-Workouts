c9114b09d36ae7bfe73af589d42bcb95068ed904
Samallll <samalchokli@gmail.com>
2023-06-30 17:41:40 +0530

    Updated

--- Assign1.java
+++ Assign1.java
@@ -4,11 +4,10 @@ class Assign1{
     public static void main(String[] args) {
 
         Scanner obj= new Scanner(System.in);
-        System.out.println("Enter the character");
+        System.out.println("Enter the character:");
 
         char name = obj.next().charAt(0);
-
-        System.out.println("Your Name is\n"+name);  
+        System.out.println("Entered character is : "+name);  
     }
 
 
